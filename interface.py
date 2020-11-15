97d59ef77bc1eeb72d37fae776da0bc6e3c7da23
jWang <m13121269577@163.com>
2020-11-15 13:32:58 +0800

    tkinter error when import pluck.py

---
+++ interface.py
@@ -0,0 +1,25 @@
+import play.pluck
+import tkinter as tk
+
+def pri(text="hhhhhhhhh"):
+    print(text)
+
+window = tk.Tk()
+# frame_border_effects = {
+#     "flat": tk.FLAT,
+#     "sunken": tk.SUNKEN,
+#     "raised": tk.RAISED,
+#     "groove": tk.GROOVE,
+#     "ridge": tk.RIDGE,
+# }
+#frame = tk.Frame(relief=tk.GROOVE, borderwidth=5)
+
+#label = tk.Label(master=frame,text="Hello, Tkinter",fg="white",bg="black",width=10,height=10)    
+#button = tk.Button(master=frame,text="Click me!",width=25,height=5,command=pri)
+#entry = tk.Entry(master=frame,fg="yellow", bg="blue", width=50)
+#label.pack()
+#button.pack()
+#entry.pack()
+#frame.pack()
+window.mainloop()
+
