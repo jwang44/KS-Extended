efd2c98689b31287aa3fea9abc486694b4cff7c0
jWang <m13121269577@163.com>
2020-12-04 17:20:11 +0800

    tidy up, delete unused functions

--- interface.py
+++ interface.py
@@ -1,81 +1,84 @@
 import pluck
 import tkinter as tk
 import pyaudio
-"""
-6 strings interface, no dist, no bend
-"""
-def change_tone(new_tone):
-    tone = new_tone
+
+# simple tk interface, depends on pluck.py
 
 p = pyaudio.PyAudio()
 # def callback(in_data, frame_count, time_info, status):
 #     return in_data, pyaudio.paContinue
+
+# open pyaudio stream
 stream = p.open(format=p.get_format_from_width(2),
                 channels=1,
                 rate=44100,
                 output=True)
 
+# start interface
 window = tk.Tk()
-frame_border_effects = {
-    "flat": tk.FLAT,
-    "sunken": tk.SUNKEN,
-    "raised": tk.RAISED,
-    "groove": tk.GROOVE,
-    "ridge": tk.RIDGE,
-}
+
+# bool parameters for checkboxes
+fb_bool = tk.BooleanVar()
+bend_bool = tk.BooleanVar()
+
 frame = tk.Frame(relief=tk.GROOVE, borderwidth=5)
 label = tk.Label(master=frame,text="Hello GuitarDemo",fg="white",bg="black",width=25,height=2)
-entry = tk.Entry(master=frame,fg="yellow", bg="blue", width=25)
-tone_scale = tk.Scale(master=frame, from_=10, to=100, orient=tk.HORIZONTAL, label='tone', length=425, showvalue=0)
-gain_scale = tk.Scale(master=frame, from_=1, to=100, orient=tk.HORIZONTAL, label='gain', width=25, showvalue=0)
 
+fb_check = tk.Checkbutton(frame, text='feedback', variable=fb_bool)
+bend_check = tk.Checkbutton(frame, text='bend', variable=bend_bool)
 
-btn1 = tk.Button(
+freq_scale = tk.Scale(master=frame, from_=82, to=440, orient=tk.HORIZONTAL, label='freq', length=425)
+dur_scale = tk.Scale(master=frame, from_=300, to=800, orient=tk.HORIZONTAL, label='dur', length=425, showvalue=0)
+tone_scale = tk.Scale(master=frame, from_=0, to=100, orient=tk.HORIZONTAL, label='tone', length=425, showvalue=0)
+gain_scale = tk.Scale(master=frame, from_=1, to=20, orient=tk.HORIZONTAL, label='gain', length=425, showvalue=0)
+freq2_scale = tk.Scale(master=frame, from_=82, to=440, orient=tk.HORIZONTAL, label='bend to', length=425)
+
+pluck_btn = tk.Button(
     master=frame,text="pluck",width=25,height=3,
     command=lambda: stream.write(
-        pluck.pluck_note(
-            freq=82, 
-            tone=tone_scale.get(), 
-            dist=True,
-            gain=gain_scale.get()
+        pluck.pluck(
+            freq=freq_scale.get(), 
+            dur=dur_scale.get()/100,
+            tone=tone_scale.get(),
+            gain=gain_scale.get(),
+            bend=bend_bool.get(),
+            bend_to=freq2_scale.get(),
+            feedback=fb_bool.get()
             )
         )
     )
-btn2 = tk.Button(
-    master=frame,text="pluck",width=25,height=3,
-    command=lambda: stream.write(pluck.pluck_note(freq=110, tone=tone_scale.get()))
-    )
-btn3 = tk.Button(
-    master=frame,text="pluck",width=25,height=3,
-    command=lambda: stream.write(pluck.pluck_note(freq=147, tone=tone_scale.get()))
-    )
-btn4 = tk.Button(
-    master=frame,text="pluck",width=25,height=3,
-    command=lambda: stream.write(pluck.pluck_note(freq=196, tone=tone_scale.get()))
-    )
-btn5 = tk.Button(
-    master=frame,text="pluck",width=25,height=3,
-    command=lambda: stream.write(pluck.pluck_note(freq=247, tone=tone_scale.get()))
-    )
-btn6 = tk.Button(
-    master=frame,text="pluck",width=25,height=3,
-    command=lambda: stream.write(pluck.pluck_note(freq=330, tone=tone_scale.get()))
+
+chord_btn = tk.Button(
+    master=frame,text="pluck chord",width=25,height=3,
+    command=lambda: stream.write(
+        pluck.pluck_chord(
+            freq=freq_scale.get(), 
+            dur=dur_scale.get()/100,
+            tone=tone_scale.get(),
+            gain=gain_scale.get(),
+            bend=bend_bool.get(),
+            bend_to=freq2_scale.get(),
+            feedback=fb_bool.get()
+            )
+        )
     )
 
 label.pack()
-btn1.pack()
-btn2.pack()
-btn3.pack()
-btn4.pack()
-btn5.pack()
-btn6.pack()
-entry.pack()
+pluck_btn.pack()
+chord_btn.pack()
+
+fb_check.pack()
+bend_check.pack()
+freq_scale.pack()
+dur_scale.pack()
 tone_scale.pack()
 gain_scale.pack()
+freq2_scale.pack()
+
 frame.pack()
 window.mainloop()
 
 stream.stop_stream()
 stream.close()
 p.terminate()
-print("closed")
+#print("closed")
--- pluck.py
+++ pluck.py
@@ -1,150 +1,20 @@
 import numpy as np
 from scipy.signal import lfilter
-from collections import deque
+#from collections import deque
 import random
 random.seed(10)
 
-def pluck_note(freq=440, dur=5, velocity=100, tone=100, dist=True, gain=30, bend=True, bend_to=500, feedback=True, fb_ratio=0.4):
+def pluck(freq=440, dur=6, velocity=127, tone=100, gain=1.5, bend=False, bend_to=500, feedback=False, fb_ratio=0.4):
     """
+    freq: fundamental frequency
+    dur: note duration in seconds
     velocity: 0-127
-    tone: 0-100
-    dist: bool
-    gain: gain amount
-    """
-    fs = 44100
-    N = round(fs * dur) # no. of samples
-    frac_delay = fs/freq - 1
-    delay = int(frac_delay) # wavtable length
-
-    # linear interp coefficients
-    c1 = frac_delay - delay # fractional part
-    c0 = 1 - c1
-
-    # ring buffer
-    rand_init = [(random.random() - 0.5) * velocity/127 for i in range(delay)]
-
-    # tone control
-    a = 1 / (2*np.cos(2*np.pi*freq/fs)+1)
-    passes = int(-0.3 * tone + 30); # map tone(0~100) to pass(100~0)
-    for i in range(passes):
-        rand_init = lfilter([a,a,a], 1, rand_init)    
-    
-    # kill dc
-    for item in rand_init:
-        item = item - sum(rand_init)/len(rand_init)
-    # dc-blocking parameters
-    dc_block_co = 2*np.pi*freq/fs/10
-    dc_block_a0 = 1/(1+dc_block_co/2)
-    dc_block_a1 = -dc_block_a0
-    dc_block_b1 = dc_block_a0 * (1 - dc_block_co/2)
-
-    # initiate delayline
-    buf = deque(rand_init)
-    # initiate feedback delay line
-    if feedback:
-        fb_delay = int(delay*fb_ratio)
-        fb_buf = deque(np.zeros(fb_delay))
-
-    note = np.zeros(N)
-    a0 = 0.05; a1 = 0.9  # 3-point averaging coefficients 
-    y_avg = 0; ym1 = 0; y_avg_m1=0; y_block_m1=0
-
-    if bend:
-        if bend_to > freq:
-            bend_amount = frac_delay - fs/bend_to
-            bend_incre = bend_amount / (0.5*fs)
-        else:
-            bend_amount = fs/bend_to - frac_delay
-            bend_incre = bend_amount / (0.5*fs)
-        incre_no = 0
-
-    for i in range(N):
-        # pitch bend control
-        if bend:
-            if bend_to > freq:
-                c1 = c1 - bend_incre
-                if c1 < 0:
-                    c1 = c1 + 1
-                    buf.popleft() # delayline length decrease by one
-                c0 = 1 - c1
-            else:
-                c1 = c1 + bend_incre
-                if c1 > 1:
-                    c1 = c1 - 1
-                    buf.append(0) # delayline length increase by one
-                    # produce glitch in sound
-            incre_no = incre_no + 1
-
-            if incre_no * bend_incre >= bend_amount:
-                bend = False
-
-        # *0.99 help decay
-        
-        y_avg = (a0*buf[0] + a1*buf[1] + a0*buf[2]) #* 0.998
-        y_block = dc_block_a0*y_avg + dc_block_a1*y_avg_m1 + dc_block_b1*y_block_m1
-        y_avg_m1 = y_avg
-        y_block_m1 = y_block
-        note[i] = c0 * y_block + c1 * ym1 # read from delayline
-        ym1 = y_block
-
-        buf.append(note[i]) # write into delayline
-        buf.popleft()
-
-        # distortion control (sample by sample)
-        note[i] = note[i] * gain
-        if dist:
-            if note[i] > 1:
-                note[i] = 2/3
-            elif note[i] < -1:
-                note[i] = -2/3
-            else:
-                note[i] = note[i] - note[i]**3 / 3
-        if feedback:
-            buf[0] = buf[0] + fb_buf[0]
-            fb_buf.append(0.01*note[i])
-            fb_buf.popleft()
-
-    note = note / max(max(note), abs(min(note))) # normalize
-
-    # distortion control
-    # if dist:
-    #     note = note * gain
-    #     dist_note = note - note**3/3
-    #     dist_note[note>1] = 2/3
-    #     dist_note[note<-1] = -2/3
-    #     # convert to 16bit, for pyaudio to play
-    #     note = np.array(dist_note * 32767, 'int16')
-    # else:
-    #     note = np.array(note * 32767, 'int16') 
-    note = np.array(note * 32767, 'int16') 
-    return note
-
-def pluck_chord(freqs=[70, 120, 140], dur=3, velocity=100, tone=100, dist=False, gain=30):
-    fs = 44100
-    N = round(dur * fs)
-    chord = np.zeros(N)
-    for freq in freqs:
-        note = pluck_note(freq, dur, velocity, tone, dist=False)
-        chord = chord + note
-    chord = chord / max(max(chord), abs(min(chord)))
-
-    if dist:
-        chord = chord * gain
-        dist_chord = chord - chord**3/3
-        dist_chord[chord>1] = 2/3
-        dist_chord[chord<-1] = -2/3
-        # convert to 16bit, for pyaudio to play
-        chord = np.array(dist_chord * 32767, 'int16') 
-    else:
-        chord = np.array(chord * 32767, 'int16') 
-
-    return chord
-
-def pluck_matlab(freq=440, dur=6, velocity=127, tone=100, gain=1.5, bend=False, bend_to=500, feedback=False, fb_ratio=0.4):
-    """
-    velocity: 0-127
-    tone: 0-100
+    tone: 0-100, controls brightness of the sound
     gain: 1-20, 1 for non-distorted
+    bend: Boolean, if false, bend_to has no effect
+    bend_to: when bend is True, bend to this frequency
+    feedback: Boolean, if false, feedback_ratio has no effect
+    fb_ratio: ratio between feedback delay and delay in the string model
     """
     fs = 44100
     N = round(fs * dur) # no. of samples
@@ -178,7 +48,6 @@ def pluck_matlab(freq=440, dur=6, velocity=127, tone=100, gain=1.5, bend=False, 
     a0 = 0.05; a1 = 0.9  
 
     # dc-blocking parameters
-    # dc_block_co = 2*np.pi*freq/fs/10
     dc_block_co = freq/fs/10
     dc_block_a0 = 1/(1+dc_block_co/2)
     dc_block_a1 = -dc_block_a0
@@ -225,14 +94,14 @@ def pluck_matlab(freq=440, dur=6, velocity=127, tone=100, gain=1.5, bend=False, 
                     c1 = c1 - 1
                     dl.append(0) # delayline length increase by one
                     D = D + 1
-                    # produce glitch in sound
+
             c0 = 1 - c1
             incre_no = incre_no + 1
 
+            # if target freq is reached, stop changing delay length
             if incre_no * bend_incre >= bend_amount:
                 bend = False
 
-        # *0.99 help decay
         if ptr > D-1:
             ptr = 0
         x = dl[ptr]
@@ -258,9 +127,15 @@ def pluck_matlab(freq=440, dur=6, velocity=127, tone=100, gain=1.5, bend=False, 
             note[i] = -2/3
         else:
             note[i] = y - y**3 / 3
+
+        # feedback control
         if feedback:
+            # add to string delay line
             dl[ptr] = dl[ptr] + dl_fb[ptr_fb]
+
+            # 0.01 attenuates the output
             dl_fb[ptr_fb] = 0.01*note[i]
+
             ptr_fb = ptr_fb + 1
             if ptr_fb > delay_fb-1:
                 ptr_fb = 0
@@ -269,14 +144,27 @@ def pluck_matlab(freq=440, dur=6, velocity=127, tone=100, gain=1.5, bend=False, 
         if ptr > D-1:
             ptr = 0
 
+    # scale for pyaudio
     note = np.array(note * 32767, 'int16') 
     return note
 
-def pluck_chord_matlab(freq=130.8, dur=5, velocity=127, tone=100, gain=1, bend=False, bend_to=500, feedback=False, fb_ratio=0.4):
+def pluck_chord(freq=130.8, dur=5, velocity=127, tone=100, gain=1, bend=False, bend_to=500, feedback=False, fb_ratio=0.4):
+    """
+    freq: fundamental frequency for the root
+    dur: note duration in seconds
+    velocity: 0-127
+    tone: 0-100, controls brightness of the sound
+    gain: 1-20, 1 for non-distorted
+    bend: Boolean, if false, bend_to has no effect
+    bend_to: when bend is True, bend to this frequency
+    feedback: Boolean, if false, feedback_ratio has no effect
+    fb_ratio: ratio between feedback delay and delay in the string model
+    """  
     fs = 44100
     N = round(dur * fs)
     chord = np.zeros(N)
-    # just intonation freq ratio 4:5:6
+
+    # just intonation freq ratio 4:5:6, form the chord
     freqs = [0,0,0]
     freqs[0] = freq
     freqs[1] = freq*1.25
@@ -287,16 +175,20 @@ def pluck_chord_matlab(freq=130.8, dur=5, velocity=127, tone=100, gain=1, bend=F
     bends_to[1] = bend_to*1.25
     bends_to[2] = bend_to*1.5
 
+    # pluck every note, if bend=False, the bends_to has no effect
     for freq, bend_to in zip(freqs, bends_to):
-        note = pluck_matlab(freq, dur, velocity, tone, gain=gain, bend=bend, bend_to=bend_to, feedback=feedback)
+        note = pluck(freq, dur, velocity, tone, gain=gain, bend=bend, bend_to=bend_to, feedback=feedback, fb_ratio=fb_ratio)
         chord = chord + note
+
     chord = chord / max(max(chord), abs(min(chord)))
 
-    #if dist:
+    # distortion
     chord = chord * gain
     dist_chord = chord - chord**3/3
     dist_chord[chord>1] = 2/3
     dist_chord[chord<-1] = -2/3
+
     # convert to 16bit, for pyaudio to play
     chord = np.array(dist_chord * 32767, 'int16') 
+
     return chord
